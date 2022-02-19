bc4bd94666eda680498ce437a8e3aef2d8869a74
mayuriaskar <98272902+mayuriaskar@users.noreply.github.com>
2022-02-19 16:50:53 +0530

    intial commit of math.py

---
+++ math.py
@@ -0,0 +1,13 @@
+
+ # inital commet
+ def add (x,y) :
+       pass
+       this is for subtract
+ def  subtract (x,y) :
+       pass
+        this is for multiply
+ def  multiply (x,y) :
+       pass
+       
+ def divide (x,y) :
+       pass   
